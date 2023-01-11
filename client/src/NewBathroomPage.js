998a88ddc461730f9ac4e4e22806eecaf406ad83
Alex Brian Naughton <alexbriannaughton@gmail.com>
2023-01-10 19:19:12 -0800

    new bathroom form--need to be logged in

--- client/src/NewBathroomPage.js
+++ client/src/NewBathroomPage.js
@@ -3,7 +3,6 @@ import BathroomForm from "./components/BathroomForm"
 function NewBathroomPage({user, APIKey, setBathrooms}) {
     return(
         <div>
-            <h1 id="AddBathroomTitle">Add a new bathroom</h1>
             <BathroomForm setBathrooms={setBathrooms} user={user} APIKey={APIKey}/>
         </div>
     )
